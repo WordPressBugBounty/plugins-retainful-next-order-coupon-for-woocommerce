1e3a722eb0888b86d997e51fd632bd6540f9a4f8
Sean Murphy <sean@iamseanmurphy.com>
2025-04-28 07:33:19 +0000

    2.6.40

--- retainful-next-order-coupon-for-woocommerce/vendor/composer/autoload_real.php
+++ retainful-next-order-coupon-for-woocommerce/vendor/composer/autoload_real.php
@@ -25,11 +25,30 @@ class ComposerAutoloaderInit88f9ddb7a9be127f3312cd3d288b5aac
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit88f9ddb7a9be127f3312cd3d288b5aac', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit88f9ddb7a9be127f3312cd3d288b5aac', 'loadClassLoader'));
 
-        require __DIR__ . '/autoload_static.php';
-        call_user_func(\Composer\Autoload\ComposerStaticInit88f9ddb7a9be127f3312cd3d288b5aac::getInitializer($loader));
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoload\ComposerStaticInit88f9ddb7a9be127f3312cd3d288b5aac::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+        }
 
         $loader->register(true);
 
